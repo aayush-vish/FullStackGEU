adfc5168ade29d19b5425b4d999373c9d3dd7d6c
Aayush Vishnoi <56499156+aayush-vish@users.noreply.github.com>
2021-01-28 04:58:21 +0000

    Added CheckBox and JS

--- PHPForm.php
+++ PHPForm.php
@@ -1,59 +1,80 @@
 <html>
+
 <head>
-<title>Welcome to PHP</title>
-<style>
-.form{
-            border: 2px solid black;
-            width: 60%;
-            padding: 20px;
-            margin-left:250px;
-            margin-top:50px;
-            border-radius:20px;
-        }
-        h1{
-            text-align:center;
-        }
-        form{
-            font-weight: 600;
-        }
-        form input{
-            border: 1px solid blue;
-            padding: 10px;
-            border-radius: 7px;
-            margin-left: 40px;
-        }
-        form input:first-child{
-            margin-left: 45px;            
-        }
-        table{
-            margin-top:50px;
-            margin-left:250px;
-            /* display:none; */
-        }
-        table,th,td{
-            border-collapse:collapse;
-            border:1px solid black;
-            padding:10px;
-        }
-        th{
-            colspan:2;
-        }
-</style>
+    <title>Welcome to PHP</title>
+    <style>
+    .form {
+        border: 2px solid black;
+        width: 60%;
+        padding: 20px;
+        margin-left: 250px;
+        margin-top: 50px;
+        border-radius: 20px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    form {
+        font-weight: 600;
+    }
+
+    form input {
+        border: 1px solid blue;
+        padding: 10px;
+        border-radius: 7px;
+        margin-left: 40px;
+    }
+
+    form input:first-child {
+        margin-left: 45px;
+    }
+
+    table {
+        margin-top: 50px;
+        margin-left: 250px;
+        /* display:none; */
+    }
+
+    table,
+    th,
+    td {
+        border-collapse: collapse;
+        border: 1px solid black;
+        padding: 10px;
+    }
+
+    th {
+        colspan: 2;
+    }
+    </style>
 </head>
+
 <body>
-<div class="form">
-    <h1>Application Form</h1>
-<form method="GET">
-        NAME <input type="text" name="name" placeholder="Enter your Name" required><br><br>
-        EMAIL <input type="email" name="mail" placeholder="Enter your Email" required><br><br>
-        CONTACT <input type="text" name="contact" placeholder="Enter your Contact" required style="margin-left:15px;"><br><br>
-        CITY <input type="text" name="city" placeholder="Enter your City" required style="margin-left:60px;"><br><br>
-        COURSE <input type="text" name="course" placeholder="Enter your course" required style="margin-left:28px;"><br><br>
-        INTEREST <input type="text" name="interest" placeholder="Enter your Interests" required style="margin-left:10px;"><br><br>
-        <input type="submit" value="Submit" name="submit" id="btn" style="margin-left:150px;">
-    </form>
-</div>     
-<?php
+    <div class="form">
+        <h1>Application Form</h1>
+        <form method="GET">
+            NAME <input type="text" name="name" placeholder="Enter your Name" required><br><br>
+            EMAIL <input type="email" name="mail" placeholder="Enter your Email" required><br><br>
+            CONTACT <input type="text" name="contact" placeholder="Enter your Contact" required minlength="10" maxlength="10"
+                style="margin-left:15px;"><br><br>
+            CITY <input type="text" name="city" placeholder="Enter your City" required
+                style="margin-left:60px;"><br><br>
+            COURSE <input type="text" name="course" placeholder="Enter your course" required
+                style="margin-left:28px;"><br><br>
+            INTEREST <input type="checkbox" id="basketball" name="interest[]" value="Basketball" style="margin-left:10px;">
+            <label for="basketball">BasketBall</label> 
+            <input type="checkbox" id="gaming" name="interest[]"  value="Gaming" style="margin-left:10px;">
+            <label for="gaming">Gaming</label>
+            <input type="checkbox" id="pentesting" name="interest[]"  value="Pentesting" style="margin-left:10px;">
+            <label for="pentesting">Pentesting</label>
+            <input type="checkbox" id="gym" name="interest[]" value="Gym" style="margin-left:10px;">
+            <label for="gym">Gym</label><br><br>
+            <input type="submit" value="Submit" name="submit" id="btn" style="margin-left:150px;">
+        </form>
+    </div>
+    <?php
 if(isset($_GET['submit'])){
     $name=$_GET['name'];
     $email=$_GET['mail'];
@@ -61,17 +82,21 @@ if(isset($_GET['submit'])){
     $city=$_GET['city'];
     $course=$_GET['course'];
     $interest=$_GET['interest'];
-    echo "<table><tr><th >Name</th><th>Email</th><th>Contact</th><th>City</th><th>Course</th><th>Interest</th></tr>
-    <tr><td>$name</td><td>$email</td><td>$contact</td><td>$city</td><td>$course</td><td>$interest</td></tr></table>";
+    foreach($interest as $selected){
+        // echo "<p>".$selected."</p>";
+    }
+    echo "<table><tr><th>Name</th><th>Email</th><th>Contact</th><th>City</th><th>Course</th><th>Interest</th></tr>
+    <tr><td>$name</td><td>$email</td><td>$contact</td><td>$city</td><td>$course</td><td>$selected</td></tr></table>";
 }
 ?>
-<script>
-var btn=document.getElementById('btn');
-btn.addEventListener('click',function(){
-if(table.style.display==="none"){
-    table.style.display="block";
-}
-})
-</script>
+    <script>
+    var btn = document.getElementById('btn');
+    btn.addEventListener('click', function() {
+        if (table.style.display === "none") {
+            table.style.display = "block";
+        }
+    })
+    </script>
 </body>
+
 </html>
